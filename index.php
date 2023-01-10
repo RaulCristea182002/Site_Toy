cd9af559676e158ba9a5da3081f44630397104d7
RaulCristea182002 <118883109+RaulCristea182002@users.noreply.github.com>
2023-01-10 14:00:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Toy Store</title>
+</head>
+<link rel="stylesheet" href="Style.css">
+<body>
+  <div class="header">
+    <div class="sus">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="AboutUs.php">About Us</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="signup.php">Signup</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+  </div>
+
+  <div class="text">
+    <p>Te pot ajuta</p>
+    <h1>Sa gasesti cele mai de top jucarii</h1>
+  </div>
+  
+</body>
+</html>
